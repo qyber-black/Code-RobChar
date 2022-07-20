d3e42984bbd0fa5bafa745a4e11a7d5c9bda4c65
erg0dic <46654174+erg0dic@users.noreply.github.com>
2022-07-20 16:35:32 +0000

    Update rename_files.py

--- rename_files.py
+++ rename_files.py
@@ -5,13 +5,13 @@ import os
 def filter_files_expression(special_char):
     return f"(_\d_\d-{special_char}\d_)"
 
-def change_given_files(allfilenames, special_char):
+def change_given_files(allfilenames, special_char, replace_char=""):
     regex_str = filter_files_expression(special_char)
     ref = lambda file: re.search(regex_str, file)
     rexfiles =  list(filter(ref, allfilenames))
 
     for file in rexfiles:
-        new_file_name = re.sub(special_char, "", file)
+        new_file_name = re.sub(special_char, replace_char, file)
         os.rename(file, new_file_name)
 
 def change_filenames(special_char=">"):
